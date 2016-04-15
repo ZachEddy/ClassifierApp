13dd45dcfe9177eac8229bed3246006097f15dff
ZachEddy <zeddy@pugetsound.edu>
2016-04-15 15:00:08 -0700

    network functions with the app; now able to submit images to app via ajax

--- classifier/static/js/image_upload.js
+++ classifier/static/js/image_upload.js
@@ -1,14 +1,65 @@
 var cropper
+
+
+
+var randomScalingFactor = function(){ return Math.round(Math.random()*100)};
+
+var barChartData = {
+
+		labels : ["Airplane","Automobile","Bird","Cat","Deer","Dog","Frog", "Horse", "Ship", "Truck"],
+		datasets : [
+			{
+				// fillColor : "rgba(61,94,244,0.65)",
+				fillColor : "rgba(194,229,242,0.65)",
+				strokeColor : "rgba(0,0,0,0.4)",
+				highlightFill: "rgba(156,156,156,0.65)",
+				highlightStroke: "rgba(0,0,0,0.9)",
+				data : [0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1]
+			},
+		],
+		// scaleGridLineColor : "rgba(0,0,0,0.05)",
+		scaleShowGridLines : true
+	}
+
+// function to load file
 var loadFile = function(event) {
 	var reader = new FileReader();
 	reader.onload = function() {
 		var output = document.getElementById('output');
 		output.src = reader.result;
+		// $('.container').css('height', '100%');
+		var containerheight = $(".container").height();
+		var imageheight = $(".output").height();
+		$(window).resize();
+		var height = $(window).height()
+		// $('.container').css('height', 'auto');
+		// $('.container').css('ovetflow', 'auto');
+		$('.container').css('height', height);
+		var r = $('<button class="send_data btn btn-default">Classify</button>')
+		var c = $('<canvas id="chart"></canvas>')
+
+		$('.classify').append(r)
+		$('.graph_container').append(c)
+		var ctx = document.getElementById("chart").getContext("2d");
+		window.myBar = new Chart(ctx).Bar(barChartData, {
+			responsive : true,
+			maintainAspectRatio: false,
+			scaleLineColor: "rgba(0,0,0,1)",
+			scaleGridLineColor : "rgba(0,0,0,0.25)",
+			barStrokeWidth : 2,
+			barValueSpacing : 15
+		});
+		$('.upload').remove();
+		
+		$(document).resize();
+
 		makeCropper()
 	};
 	reader.readAsDataURL(event.target.files[0]);
 };
 
+// function to grab data within the crop field
+$('.classify').after(loadFile)
 var makeCropper = function() {
 	image = document.getElementById('output');
 	cropper = new Cropper(image, {
@@ -30,10 +81,9 @@ var makeCropper = function() {
 	cropper.zoomTo(1);
 };
 
+// field to pass data to the 
 $(function() {
 	var classify = function(event) {
-		console.log("Fuck")
-		
 		cropper.getCroppedCanvas({width: 32, height: 32}).toBlob(function (blob) {
 			var formData = new FormData();
 			formData.append('croppedImage', blob);
@@ -43,7 +93,16 @@ $(function() {
 				processData: false,
 				contentType: false,
 				success: function (data) {
-					console.log(data);
+					console.log("Hello");	
+					var counter = 0;				
+					for(key in data)
+					{
+						console.log(data[key])
+						console.log(counter);
+						window.myBar.datasets[0].bars[counter].value = data[key];
+						counter++;
+					}
+					window.myBar.update();
 				},
 				error: function () {
 					console.log('Upload error');
--- classifier/views.py
+++ classifier/views.py
@@ -3,23 +3,33 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 
-# Create your views here.
+from network.__main__ import classify_user_image
 
 def index(request):
 		print request
 		if request.method == 'POST':
-			print "A post request was made!"
 
 			handle_uploaded_file(request.FILES['croppedImage'])
 
-			print request.FILES['croppedImage']
-			return JsonResponse({'foo':'bar'})
+			probs = classify_user_image()
 
-		
+			return JsonResponse({
+				'Airplane': probs[0],
+				'automobile': probs[1],
+				'bird': probs[2],
+				'cat': probs[3],
+				'deer': probs[4],
+				'dog': probs[5],
+				'frog': probs[6],
+				'horse': probs[7],
+				'ship': probs[8],
+				'truck': probs[9],
+				})
+			
 		return render(request, "classifier.html", {'type':'this is a plan'})
 
 
 def handle_uploaded_file(f):
-    with open('name.png', 'wb+') as destination:
-        for chunk in f.chunks():
-            destination.write(chunk)
+	with open('image.png', 'wb+') as destination:
+		for chunk in f.chunks():
+			destination.write(chunk)
